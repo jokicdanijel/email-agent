9f3958b77a2126528c78b0086420d653d9f8f784
Danijel Jokic <xxjokic01@gmail.com>
2025-01-26 17:34:11 +0100

    Initial commit

---
+++ email_config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_USER = os.getenv("xxjokic04mail.com")
+EMAIL_PASS = os.getenv("Xx250886!")
+IMAP_SERVER = os.getenv("IMAP_SERVER", "imap.gmail.com")
+SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
